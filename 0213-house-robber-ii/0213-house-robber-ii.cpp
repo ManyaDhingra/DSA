4159cc3011aa131b2c0f92a603a8a89ab2651f86
Manya <151215790+ManyaDhingra@users.noreply.github.com>
2025-11-18 22:58:10 +0530

    Time: 0 ms (100%), Space: 10.3 MB (66.32%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int solve(vector<int>& nums){
+        int n = nums.size();
+        int prev2 = 0;
+        int prev1 = nums[0];
+
+        for (int i = 1; i<n ; i++){
+            int incl = prev2 + nums[i];
+            int excl = prev1 + 0;
+            int ans = max(incl, excl);
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return prev1;
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        vector<int> fir, sec;
+        for (int i = 0; i<n; i++){
+            if(i != n-1){
+                fir.push_back(nums[i]);
+            }
+            if(i != 0){
+                sec.push_back(nums[i]);
+            }
+        }
+
+        return max(solve(fir), solve(sec));
+    }
+};
