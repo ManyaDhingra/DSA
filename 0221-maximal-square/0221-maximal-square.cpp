921845c8ea62a30574499bd163deb5cbd8d36fe8
Manya <151215790+ManyaDhingra@users.noreply.github.com>
2025-12-02 00:37:50 +0530

    Time: 11 ms (47.47%), Space: 31.1 MB (39.08%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,19 +1,23 @@
 class Solution {
-    int solve(vector<vector<char>>& matrix, int m, int n, int i, int j, int &maxi){
+    int solve(vector<vector<char>>& matrix, int m, int n, int i, int j, int &maxi, vector<vector<int>> &dp){
         if(i>=m || j >= n){
             return 0;
         }
-        int right = solve(matrix, m, n, i+1, j, maxi);
-        int down =  solve(matrix, m, n, i, j+1, maxi);
-        int dia = solve(matrix, m, n, i+1, j+1, maxi);
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int right = solve(matrix, m, n, i+1, j, maxi, dp);
+        int down =  solve(matrix, m, n, i, j+1, maxi, dp);
+        int dia = solve(matrix, m, n, i+1, j+1, maxi, dp);
 
         if(matrix [i][j] == '1'){
-            int ans = min(right, min(down, dia))+1;
-            maxi = max(maxi, ans);
-            return ans;
+            dp[i][j] = min(right, min(down, dia))+1;
+            maxi = max(maxi, dp[i][j]);
+            return dp[i][j];
 
         }
         else{
+            dp[i][j] = 0;
             return 0 ;
         }
         
@@ -24,7 +28,8 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int maxi = 0;
-        solve(matrix, m,n, 0, 0 , maxi);
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        solve(matrix, m,n, 0, 0 , maxi, dp);
         return maxi*maxi;
     }
 };
