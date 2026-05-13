af8afb2594d5e7c0af1a7236343500825ff3681c
Manya <151215790+ManyaDhingra@users.noreply.github.com>
2026-05-13 20:38:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.19%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        long long N = n;
+
+        // handle negative exponent
+        if(N < 0){
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1;
+
+        while(N > 0){
+
+            // if odd power
+            if(N % 2 == 1){
+                ans *= x;
+            }
+
+            x *= x;
+            N /= 2;
+        }
+
+        return ans;
+    }
+};
